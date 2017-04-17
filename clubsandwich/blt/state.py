72127b8fd05b8274de99ae090925b6c0c0140523
Steve Johnson <sjohnson@hipmunk.com>
2017-04-17 11:07:44 -0700

    Change blt_state to prefix numbers with num_

--- clubsandwich/blt/state.py
+++ clubsandwich/blt/state.py
@@ -7,6 +7,9 @@
     For example, ``blt_state.shift`` returns ``True`` iff the Shift key is
     down. This is forwarded to
     ``bearlibterminal.terminal.state(terminal.TK_SHIFT)``.
+
+    Numbers are prefixed with ``num_``. So ``blt_state.num_1`` is true iff the
+    "1" key is being pressed.
 """
 from bearlibterminal import terminal
 
@@ -20,9 +23,11 @@ for constant_key in (c for c in dir(terminal) if c.startswith('TK_')):
         def get(self):
             return terminal.state(constant_value)
         return get
+    constant_name = constant_key[3:].lower()
+    attr_name = 'num_{}'.format(constant_name) if constant_name[0].isdigit() else constant_name
     setattr(
         _TerminalState,
-        constant_key[3:].lower(),
+        attr_name,
         property(getter(constant_key)))
 
 blt_state = _TerminalState()
