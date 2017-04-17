b6093db8e885f3126db8c4379956a9d2dd651fb9
Steve Johnson <steve@graffitilabs.com>
2017-04-16 17:39:50 -0700

    Hello docs

--- clubsandwich/geom.py
+++ clubsandwich/geom.py
@@ -1,6 +1,27 @@
+"""
+Simple data structures for working with points, sizes, and rects.
+"""
 from math import floor
 
 class Point:
+  """
+  :param Real x:
+  :param Real y:
+
+  Represents a point in 2D space. Supports arithmetic operations with other
+  points, and supports multiplication and division with numbers.
+
+  BearLibTerminal requires ints for all coordinate values, so
+  :py:attr:`~Point.floored` is your friend.
+
+  .. py:attribute:: x
+
+    X axis coordinate; may be any number
+
+  .. py:attribute:: y
+
+    Y coordinate; may be any number
+  """
   __slots__ = ('x', 'y')
 
   def __init__(self, x=0, y=0):
@@ -18,6 +39,7 @@ class Point:
 
   @property
   def floored(self):
+    """A copy of this point with ``math.floor()`` called on each coordinate."""
     return self.__class__(floor(self.x), floor(self.y))
 
   def __add__(self, other):
@@ -40,6 +62,22 @@ class Point:
 
 
 class Size(Point):
+  """
+  :param Real width:
+  :param Real height:
+
+  Thin wrapper over :py:class:`Point` that forwards :py:attr:`~Point.x` to
+  :py:attr:`width` and :py:attr:`~Point.y` to :py:attr:`height`.
+
+  .. py:attribute:: width
+
+    alias for :py:attr:`~Point.x`
+
+  .. py:attribute:: height
+
+    alias for :py:attr:`~Point.y`
+  """
+
   def __init__(self, width=0, height=0):
     super().__init__(x=width, y=height)
 
@@ -68,6 +106,23 @@ class Size(Point):
 
 
 class Rect:
+  """
+  :param Point origin:
+  :param Size size:
+
+  Represents an rectangle in 2D space.
+
+  BearLibTerminal requires ints for all coordinate values, so
+  :py:attr:`~Rect.floored` is your friend.
+
+  .. py:attribute:: origin
+
+    Origin of the rectangle
+
+  .. py:attribute:: size
+
+    Size of the rectangle
+  """
   __slots__ = ('origin', 'size')
 
   def __init__(self, origin=None, size=None):
@@ -85,6 +140,7 @@ class Rect:
 
   @property
   def x(self):
+    """Forwarded from ``self.origin.x``"""
     return self.origin.x
 
   @x.setter
@@ -93,6 +149,7 @@ class Rect:
 
   @property
   def y(self):
+    """Forwarded from ``self.origin.y``"""
     return self.origin.y
 
   @y.setter
@@ -101,6 +158,7 @@ class Rect:
 
   @property
   def width(self):
+    """Forwarded from ``self.size.width``"""
     return self.size.width
 
   @width.setter
@@ -109,6 +167,7 @@ class Rect:
 
   @property
   def height(self):
+    """Forwarded from ``self.size.height``"""
     return self.size.height
 
   @height.setter
@@ -117,22 +176,26 @@ class Rect:
 
   @property  # no setter; it's not clear if it would change origin or size
   def x2(self):
+    """Max X value of this rect. Read-only."""
     return self.origin.x + self.size.width - 1
 
   @property  # no setter; it's not clear if it would change origin or sizey
   def y2(self):
+    """Max Y value of this rect. Read-only."""
     return self.origin.y + self.size.height - 1
 
   ### handy iterators ###
 
   @property
   def points(self):
+    """Iterator of all points in this rect"""
     for x in range(self.origin.x, self.origin.x + self.size.width):
       for y in range(self.origin.y, self.origin.y + self.size.height):
         yield Point(x, y)
 
   @property
   def points_top(self, corners=False):
+    """Iterator of all points along the top edge of this rect"""
     start = self.origin.x if corners else self.origin.x + 1
     end = self.origin.x + self.size.width if corners else self.origin.x + self.size.width - 1
     for x in range(start, end):
@@ -140,6 +203,7 @@ class Rect:
 
   @property
   def points_bottom(self, corners=False):
+    """Iterator of all points along the bottom edge of this rect"""
     start = self.origin.x if corners else self.origin.x + 1
     end = self.origin.x + self.size.width if corners else self.origin.x + self.size.width - 1
     for x in range(start, end):
@@ -147,6 +211,7 @@ class Rect:
 
   @property
   def points_left(self, corners=False):
+    """Iterator of all points along the left edge of this rect"""
     start = self.origin.y if corners else self.origin.y + 1
     end = self.origin.y + self.size.height if corners else self.origin.y + self.size.height - 1
     for y in range(start, end):
@@ -154,6 +219,7 @@ class Rect:
 
   @property
   def points_right(self, corners=False):
+    """Iterator of all points along the right edge of this rect"""
     start = self.origin.y if corners else self.origin.y + 1
     end = self.origin.y + self.size.height if corners else self.origin.y + self.size.height - 1
     for y in range(start, end):
@@ -161,6 +227,7 @@ class Rect:
 
   @property
   def points_corners(self):
+    """Iterator of all four points in this rect's corners"""
     yield self.origin
     yield self.point_top_right
     yield self.point_bottom_right
@@ -170,34 +237,63 @@ class Rect:
 
   @property
   def center(self):
+    """Point at the center of this rect"""
     return self.origin + self.size / 2
 
   @property
   def point_top_right(self):
+    """Point at the top right corner of this rect"""
     return Point(self.origin.x + self.size.width - 1, self.origin.y)
   
   @property
   def point_bottom_left(self):
+    """Point at the bottom left corner of this rect"""
     return Point(self.origin.x, self.origin.y + self.size.height - 1)
 
   @property
   def point_bottom_right(self):
+    """Point at the bottom right corner of this rect"""
     return self.origin + self.size - Point(1, 1)
 
   ### copying transforms ###
 
   @property
   def floored(self):
+    """A copy of this rect with the :py:attr:`Point.floored` origin/size
+    copies"""
     return Rect(self.origin.floored, self.size.floored)
 
   def moved_by(self, delta):
+    """
+    :param Point delta:
+
+    A copy of this rect with the origin moved by **delta**
+    """
     return Rect(self.origin + delta, self.size)
 
   def with_origin(self, new_origin):
+    """
+    :param Point new_origin:
+
+    A copy of this rect with the given origin
+    """
     return Rect(new_origin, self.size)
 
   def with_size(self, new_size):
+    """
+    :param Point new_size:
+
+    A copy of this rect with the given size
+    """
     return Rect(self.origin, new_size)
 
   def with_inset(self, inset):
+    """
+    :param Point|Real inset:
+
+    A copy of this rect inset by the given amount. If you pass a number, all
+    sides will be inset by the same amount. If you pass a :py:class:`Point`
+    or a :py:class:`Size`, the X and Y axes will be inset by the respective
+    amounts specified in each coordinate.
+    """
     return Rect(self.origin + inset, self.size - inset * 2)
---
+++ docs/conf.py
@@ -0,0 +1,40 @@
+import os
+
+READ_THE_DOCS = os.environ.get('READTHEDOCS', None) == 'True'
+
+needs_sphinx = '1.5'
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage',
+              'sphinx.ext.intersphinx', 'sphinx.ext.ifconfig']
+intersphinx_mapping = {}
+
+#templates_path = ['_templates']
+exclude_patterns = ['_build']
+source_suffix = '.rst'
+master_doc = 'index'
+#pygments_style = 'sphinx'
+
+# project info
+
+project = u'clubsandwich'
+copyright = u'2017 Steve Johnson'
+# The short X.Y version. Can refer to in docs with |version|.
+version = '0.1'
+# The full version, including alpha/beta/rc tags.
+# Can refer to in docs with |release|.
+release = '0.1a1'
+#language = None
+
+html_title = "%(project)s v%(release)s docs - BearLibTerminal framework for Python" % {
+    'project': project, 'release': release}
+html_short_title = "Home"
+
+#if not READ_THE_DOCS:
+#    html_theme_options['ga_ua'] = 'UA-foo'
+#    html_theme_options['ga_domain'] = 'bar'
+
+# Necessary for best search results
+html_show_sourcelink = True
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'csdoc'
